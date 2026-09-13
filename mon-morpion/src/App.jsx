1d47594ecfc8a64c68c733ba62f125f05f2d3b51
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2026-09-13 14:03:02 +0200

    Un export
    ADD : composant d'un board
    - function Board() → crée le composant.
    - export default → permet à d'autres fichiers d'importer ce composant.
    1) xIsNext : la valeur actuelle
    2) setXIsNext : la fonction pour modifier cette valeur
    useState(true);
    C'est à X de jouer.
    
    ADD : squares
    FEAT : tu crées l'état qui contient les 9 cases du morpion.

--- mon-morpion/src/App.jsx
+++ mon-morpion/src/App.jsx
@@ -8,3 +8,11 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
+
+export default function Board() {
+  
+  const [xIsNext, setXIsNext] = useState(true);
+
+  
+  const [squares, setSquares] = useState(Array(9).fill(null));
+}

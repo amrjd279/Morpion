543a0c988362f2bb80d6576e9b5d6a62c96c53c8
amrjd279 <160243803+amrjd279@users.noreply.github.com>
2026-09-13 14:58:58 +0200

    Le composant Square avec la classe de victoire.
    ADD : un nouveau composant React `Square` qui affiche un bouton gérant les clics et applique conditionnellement une classe CSS `winning` via `isWinningSquare`. Cela permet de gérer les interactions avec le plateau et de mettre en évidence les cases gagnantes.

---
+++ mon-morpion/src/components/Square.jsx
@@ -0,0 +1,10 @@
+export default function Square({ value, onSquareClick, isWinningSquare }) {
+  return (
+    <button 
+      className={`square ${isWinningSquare ? 'winning' : ''}`} 
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
